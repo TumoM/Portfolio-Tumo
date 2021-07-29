d2b8d0f337056822f54c25408e24fed8871468f7
Tumo M <tumomasire@gmail.com>
2021-07-29 22:48:00 +0200

    Basic CV

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -2,14 +2,19 @@ import BaseLayout from 'components/layouts/BaseLayout'
 import React from "react"
 import BasePage from 'components/BasePage'
 import { useUser } from '@auth0/nextjs-auth0';
+import {Col, Row} from "reactstrap";
 
 const Cv = () =>  {
   const { user, error, isLoading } = useUser();
 
   return (
     <BaseLayout user={user} loading={isLoading}>
-        <BasePage>
-           <h1>I am Cv page</h1>
+        <BasePage header={"Tumo's CV"}>
+          <Row>
+            <Col md={{size: 8, offset: 2}}>
+              <iframe style={{width: '100%', height: '800px'}} src="/tumo_cv.pdf"/>
+            </Col>
+          </Row>
         </BasePage>
     </BaseLayout>
   )
