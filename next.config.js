d5d1bec11c3276b76e24871d19047e4ebfefccdd
Tumo M <tumomasire@gmail.com>
2021-07-30 13:42:27 +0200

    Next,config changes

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ module.exports = {
   }
   ,env: {
     AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE,
-    BASE_URL: process.env.BASE_URL
+    BASE_URL: process.env.VERCEL_URL || process.env.BASE_URL,
+    AUTH0_BASE_URL: process.env.VERCEL_URL || process.env.BASE_URL,
   }
 }
